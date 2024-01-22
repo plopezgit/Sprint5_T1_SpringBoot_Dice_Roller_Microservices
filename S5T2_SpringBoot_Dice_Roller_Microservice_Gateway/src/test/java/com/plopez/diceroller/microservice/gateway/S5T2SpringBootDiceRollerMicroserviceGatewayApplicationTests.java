0ec372ac15fd68b7a2f46c044b2bec540661b669
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-22 18:51:14 +0100

    dice roller microservices gateway

---
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Gateway/src/test/java/com/plopez/diceroller/microservice/gateway/S5T2SpringBootDiceRollerMicroserviceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plopez.diceroller.microservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5T2SpringBootDiceRollerMicroserviceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

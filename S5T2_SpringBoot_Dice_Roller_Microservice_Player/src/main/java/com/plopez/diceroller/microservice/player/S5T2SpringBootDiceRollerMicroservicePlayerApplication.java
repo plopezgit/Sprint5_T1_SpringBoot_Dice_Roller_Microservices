46f93f5d47704286a0396ccb1003200e27523114
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-26 14:16:37 +0100

    delete feign client package references

--- S5T2_SpringBoot_Dice_Roller_Microservice_Player/src/main/java/com/plopez/diceroller/microservice/player/S5T2SpringBootDiceRollerMicroservicePlayerApplication.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Player/src/main/java/com/plopez/diceroller/microservice/player/S5T2SpringBootDiceRollerMicroservicePlayerApplication.java
@@ -4,12 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class S5T2SpringBootDiceRollerMicroservicePlayerApplication {
 
 	@Bean

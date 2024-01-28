436f42e2ba3850412c73db96322bdd95544f2c11
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-28 18:33:22 +0100

    changes game-service persistence from mysql to postgresql; doc ref:https://aws.amazon.com/es/compare/the-difference-between-mysql-vs-postgresql/; test passed; now the projects persistence is: auth-service:mongodb, player-service:mysql, game-service:postgreSQL

--- S5T2_SpringBoot_Dice_Roller_Microservice_Game/src/main/java/com/plopez/diceroller/microservice/game/model/entity/Game.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Game/src/main/java/com/plopez/diceroller/microservice/game/model/entity/Game.java
@@ -5,21 +5,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//import java.util.Random;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "games")
-public class Game {
+public class Game implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private int id;
+    @Column(name="playerId")
     private int playerId;
+    @Column(name="die1")
     private int die1;
+    @Column(name="die2")
     private int die2;
+    @Column(name="result")
     private int result;
 
 }

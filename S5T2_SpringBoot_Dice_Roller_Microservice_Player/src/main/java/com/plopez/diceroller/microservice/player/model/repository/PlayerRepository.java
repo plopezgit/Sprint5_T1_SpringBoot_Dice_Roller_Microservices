059a462d26c04401d2324d2105ab8832b932dc1a
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-28 10:44:40 +0100

    yml updates for persistence tests

--- S5T2_SpringBoot_Dice_Roller_Microservice_Player/src/main/java/com/plopez/diceroller/microservice/player/model/repository/PlayerRepository.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Player/src/main/java/com/plopez/diceroller/microservice/player/model/repository/PlayerRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @SuppressWarnings("unused")
 @Repository
-public interface PlayerMySQLRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
     Optional<Player> findByNickname(String nickname);
     boolean existsByNickname(String nickname);
 }

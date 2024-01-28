55b3efd673f94b0706e0680d83452cfb5b4e2758
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-28 11:40:29 +0100

    yml updates for persistence tests

--- S5T2_SpringBoot_Dice_Roller_Microservice_AuthService/src/main/java/com/plopez/diceroller/microservice/authservice/model/repository/AuthUserRepository.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_AuthService/src/main/java/com/plopez/diceroller/microservice/authservice/model/repository/AuthUserRepository.java
@@ -1,13 +1,13 @@
 package com.plopez.diceroller.microservice.authservice.model.repository;
 
 import com.plopez.diceroller.microservice.authservice.model.entity.AuthUser;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @SuppressWarnings("unused")
 @Repository
-public interface AuthUserRepository extends MongoRepository<AuthUser, Integer> {
+public interface AuthUserRepository extends JpaRepository<AuthUser, Integer> {
     Optional<AuthUser> findByUserName(String username);
 }

e40f938200e572e63171b51b3160773590b11ebf
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-01-30 11:22:24 +0100

    develops ResponseMessage class for each microservice; it inlcudes the following informative attributes: response code, message, message description, response timestamp; Builder lombok annotation was includded to prepare its use on controllers class

--- S5T2_SpringBoot_Dice_Roller_Microservice_Player/src/main/java/com/plopez/diceroller/microservice/player/controller/ResponseMessage.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Player/src/main/java/com/plopez/diceroller/microservice/player/controller/ResponseMessage.java
@@ -1,4 +1,19 @@
 package com.plopez.diceroller.microservice.player.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ResponseMessage {
+    private int responseCode;
+    private String message;
+    private String messageDescription;
+    private Date responseTimeStamp;
 }

23a99659cdc939bc61e28d949403d6df911d2d73
Pedro Lopez <p.lopez.ch.hr@hotmail.com>
2024-02-05 13:08:14 +0100

    closes #104, #105, #107; unit and manual test passed

--- S5T2_SpringBoot_Dice_Roller_Microservice_Game/src/main/java/com/plopez/diceroller/microservice/game/S5T2SpringBootDiceRollerMicroserviceGameApplication.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Game/src/main/java/com/plopez/diceroller/microservice/game/S5T2SpringBootDiceRollerMicroserviceGameApplication.java
@@ -10,7 +10,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@OpenAPIDefinition(info = @Info(title = "Game-Service", version = "1.0.0"))
+@OpenAPIDefinition(info = @Info(title = "Game microservice", description = "Process information related to " +
+		"the Dice roller Game; It is able to connect to Player microservice player to link games with a specific player, " +
+		"keep updated player success rate information, and delete all the games of a specific player.", version = "1.0.0"))
 public class S5T2SpringBootDiceRollerMicroserviceGameApplication {
 
 	@Bean
--- S5T2_SpringBoot_Dice_Roller_Microservice_Game/src/main/java/com/plopez/diceroller/microservice/game/model/dto/GameDTO.java
+++ S5T2_SpringBoot_Dice_Roller_Microservice_Game/src/main/java/com/plopez/diceroller/microservice/game/model/dto/GameDTO.java
@@ -1,5 +1,6 @@
 package com.plopez.diceroller.microservice.game.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +12,14 @@ import lombok.NoArgsConstructor;
 @Builder
 public class GameDTO {
 
+    @Schema(description = "Entity identification.", example = "1")
     private int id;
+    @Schema(description = "Entity player correlated identification attribute.", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     private int playerId;
+    @Schema(description = "Die one roll information attribute randomly generated.", example = "1-6")
     private int die1;
+    @Schema(description = "Die two roll information attribute randomly generated.", example = "1-6")
     private int die2;
+    @Schema(description = "Result information attribute based on the sum of dice information. 0 equal to Lose. 1 equal to Win.", example = "0-1")
     private int result;
-
 }
